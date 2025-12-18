dbb54e4fc1f0a9dda8928369948788e318f2d2db
Khonsadiya Nasywa Salsabila <nasywa9905@gmail.com>
2025-12-18 21:00:59 +0700

    Membuat EntryViewModel dan inisialisasi state UI

---
+++ app/src/main/java/com/example/pertemuan12/viewmodel/EntryViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.pertemuan12.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.pertemuan12.modeldata.DetailSiswa
+import com.example.pertemuan12.modeldata.UIStateSiswa
+import com.example.pertemuan12.modeldata.toDataSiswa
+import com.example.pertemuan12.repositori.RepositoryDataSiswa
+import retrofit2.Response
+
+class EntryViewModel(private val repositoryDataSiswa: RepositoryDataSiswa):
+    ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+
+}
