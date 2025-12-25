397843c2ccb43aa406e7a0c5f81e87e05e15222e
Khonsadiya Nasywa Salsabila <nasywa9905@gmail.com>
2025-12-26 01:50:58 +0700

    Inisialisasi DetailViewModel dan UI state.

---
+++ app/src/main/java/com/example/pertemuan12/viewmodel/DetailViewModel.kt
@@ -0,0 +1,32 @@
+package com.example.pertemuan12.viewmodel
+
+import android.annotation.SuppressLint
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pertemuan12.modeldata.DataSiswa
+import com.example.pertemuan12.repositori.RepositoryDataSiswa
+import com.example.pertemuan12.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import kotlinx.serialization.InternalSerializationApi
+import retrofit2.HttpException
+import retrofit2.Response
+import java.io.IOException
+
+
+sealed interface StatusUIDetail {
+    data class Success(val satusiswa: DataSiswa) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
+}
+class DetailViewModel(savedStateHandle: SavedStateHandle,private val repositoryDataSiswa:
+RepositoryDataSiswa):ViewModel() {
+
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+    var statusUIDetail:StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
+        private set
+
+}
