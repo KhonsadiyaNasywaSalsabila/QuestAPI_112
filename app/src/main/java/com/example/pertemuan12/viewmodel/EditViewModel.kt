fbd6c9938888cafc8c41a076258f8f483c2b31df
Khonsadiya Nasywa Salsabila <nasywa9905@gmail.com>
2025-12-26 01:42:26 +0700

    Menyiapkan struktur dan variabel state EditViewModel, dan mengambil ID siswa dari navigasi.

---
+++ app/src/main/java/com/example/pertemuan12/viewmodel/EditViewModel.kt
@@ -0,0 +1,26 @@
+package com.example.pertemuan12.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pertemuan12.modeldata.DetailSiswa
+import com.example.pertemuan12.modeldata.UIStateSiswa
+import com.example.pertemuan12.modeldata.toDataSiswa
+import com.example.pertemuan12.modeldata.toUiStateSiswa
+import com.example.pertemuan12.repositori.RepositoryDataSiswa
+import com.example.pertemuan12.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import retrofit2.Response
+
+class EditViewModel(savedStateHandle: SavedStateHandle,private val repositoryDataSiswa:
+RepositoryDataSiswa
+): ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+
+}
