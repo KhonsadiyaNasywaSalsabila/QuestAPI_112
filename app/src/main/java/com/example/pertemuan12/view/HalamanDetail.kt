a93d37ca73adc80aecf78ed5aab9e7d1f79bc739
Khonsadiya Nasywa Salsabila <nasywa9905@gmail.com>
2025-12-26 02:22:42 +0700

    Screen Utama DetailSiswaScreen, dan Integrasi ViewModel

--- app/src/main/java/com/example/pertemuan12/view/HalamanDetail.kt
+++ app/src/main/java/com/example/pertemuan12/view/HalamanDetail.kt
@@ -41,6 +41,55 @@ import com.example.pertemuan12.viewmodel.StatusUIDetail
 import com.example.pertemuan12.viewmodel.provider.PenyediaViewModel
 import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3Api::class)
+@Composable
+fun DetailSiswaScreen(
+    navigateToEditItem: (Int) -> Unit,
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel .Factory)
+) {
+    Scaffold(
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiDetail.titleRes),
+                canNavigateBack = true,
+                navigateUp = navigateBack,
+            )
+        },
+        floatingActionButton = {
+            val uiState = viewModel.statusUIDetail
+            FloatingActionButton(
+                onClick = {
+                    when(uiState){ is StatusUIDetail.Success ->
+                        navigateToEditItem(uiState.satusiswa.id) else->{}}
+
+                },
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large))
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = stringResource(R.string.update),
+                )
+            }
+        }, modifier = modifier
+    ) { innerPadding ->
+        val coroutineScope = rememberCoroutineScope()
+        BodyDetailDataSiswa(
+            statusUIDetail = viewModel.statusUIDetail,
+            onDelete = { coroutineScope.launch {
+                viewModel.hapusSatuSiswa()
+                navigateBack()
+            } },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+        )
+    }
+}
+
+
 @Composable
 private fun BodyDetailDataSiswa(
     statusUIDetail: StatusUIDetail,
